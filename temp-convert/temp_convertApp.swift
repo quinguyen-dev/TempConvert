3182dab73729fdbe4b282aae28f752c16e3a7302
Qui Nguyen <quinguyen.dev@gmail.com>
2023-10-30 16:26:46 -0400

    Initial Commit

---
+++ temp-convert/temp_convertApp.swift
@@ -0,0 +1,17 @@
+//
+//  temp_convertApp.swift
+//  temp-convert
+//
+//  Created by Qui Nguyen on 10/30/23.
+//
+
+import SwiftUI
+
+@main
+struct temp_convertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
